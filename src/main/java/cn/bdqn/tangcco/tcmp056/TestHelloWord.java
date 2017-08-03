f7306d34e25cdfcbca7eb41bb70d87289596d3a2
wsjavagit <1622992120@qq.com>
2017-08-04 01:36:35 +0800

    第二次更新wangshuo01到远程仓库

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/TestHelloWord.java
@@ -0,0 +1,15 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/4/004.
+ */
+public class TestHelloWord {
+
+    @Test
+    public void TestHello() {
+        System.out.println("Hello Word");
+    }
+
+}
